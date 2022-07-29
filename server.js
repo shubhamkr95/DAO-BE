8e5d6ba35f898d9e7b0e4699d7364a114f71d7cc
shubhamkr95 <sahooshubham124@gmail.com>
2022-07-29 13:15:12 +0530

    added routes

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const cors = require("cors");
 const { ethers } = require("ethers");
 const { proposalDetails } = require("./models/proposalDetails");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/api/proposal_hash", async (req, res, next) => {
+app.get("/api/", async (req, res, next) => {
  const data = await proposalDetails.find();
  const result = data.map((item) => ({
   objId: item._id,
@@ -24,50 +25,56 @@ app.get("/api/proposal_hash", async (req, res, next) => {
   endBlock: item.endBlock,
  }));
  res.status(200).json(result);
-
  next();
 });
+// ============================================================================================================
+app.get("/api/views/:id", async (req, res, next) => {
+ try {
+  const objId = mongoose.Types.ObjectId(req.params.id.trim());
+  const result = await proposalDetails.findById(objId);
+  res.status(200).json(result);
+ } catch (error) {
+  console.log(error);
+ }
+});
+// ============================================================================================================
 
 app.post("/api/create", async (req, res, next) => {
- const provider = new ethers.providers.JsonRpcProvider("https://rpc-mumbai.maticvigil.com/");
-
- // console.log(req.body.proposal_hash);
-
- const txnHash = {
-  proposal_hash: req.body.proposal_hash,
- };
-
- await proposalData.create(txnHash);
-
- const dataID = await proposalData.find().limit(1).sort({ $natural: -1 });
-
- console.log(dataID);
-
- const events = await provider.getTransactionReceipt(dataID[0].proposal_hash);
-
- const logs = events.logs[0].data;
- const decodeData = ethers.utils.defaultAbiCoder.decode(
-  ["uint256", "address", "address[]", "uint256[]", "string[]", "bytes[]", "uint256", "uint256", "string"],
-  logs
- );
-
- const proposerAddress = decodeData[1];
- const ID = decodeData[0].toString();
- const desc = decodeData[8];
- const startBlock = decodeData[6].toString();
- const endBlock = decodeData[7].toString();
-
- const ProposalData = {
-  proposer_address: proposerAddress,
-  proposal_id: ID,
-  proposal_description: desc,
-  startBlock: startBlock,
-  endBlock: endBlock,
- };
-
  try {
+  const txnHash = {
+   proposal_hash: req.body.hash,
+  };
+  await proposalData.create(txnHash);
+
+  // setTimeout(async () => {
+  const provider = new ethers.providers.JsonRpcProvider("https://rpc-mumbai.maticvigil.com/");
+
+  const dataID = await proposalData.find().limit(1).sort({ $natural: -1 });
+
+  const events = await provider.getTransactionReceipt(dataID[0].proposal_hash);
+  const logs = events.logs[0].data;
+  const decodeData = ethers.utils.defaultAbiCoder.decode(
+   ["uint256", "address", "address[]", "uint256[]", "string[]", "bytes[]", "uint256", "uint256", "string"],
+   logs
+  );
+
+  const proposerAddress = decodeData[1];
+  const ID = decodeData[0].toString();
+  const desc = decodeData[8];
+  const startBlock = decodeData[6].toString();
+  const endBlock = decodeData[7].toString();
+
+  const ProposalData = {
+   proposer_address: proposerAddress,
+   proposal_id: ID,
+   proposal_description: desc,
+   startBlock: startBlock,
+   endBlock: endBlock,
+  };
+
   await proposalDetails.create(ProposalData);
   res.status(200).send("Proposal hash has been stored to DB");
+  // }, 2000);
  } catch (error) {
   res.status(500).send(error);
  }
